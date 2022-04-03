8fbe98b6d6de8b4111611e27a30e9fd05ca12458
r-elen <elen.ryazanova@gmail.com>
2022-04-03 13:49:43 +0300

    finish part 3 03.04.22

--- Занятие3/Домашнее_задание/task3/main.py
+++ Занятие3/Домашнее_задание/task3/main.py
@@ -1,10 +1,18 @@
 def get_max_length_word(str_words):
-    ...  # TODO получить список непустых слов
+    words_list = str_words.split()  # TODO получить список непустых слов
 
     max_len = 0  # пусть изначально длина самого длинного слова равна 0
-    ...  # TODO пройти по всем словам и найти самую большую длину
+    for word in words_list:
+        if len(word) > max_len:
+            max_len = len(word)
 
-    ...  # TODO составить список из слов, длина которых равна самой большой
+    # TODO составить список из слов, длина которых равна самой большой
+    list_max_words = []
+    for word in words_list:
+        if len(word) == max_len:
+            list_max_words.append(word)
+
+    return list_max_words
 
 
 if __name__ == "__main__":
