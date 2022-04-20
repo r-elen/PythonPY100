280aed9b2b77fbba9bc179f5196f9ad6a449e223
r-elen <elen.ryazanova@gmail.com>
2022-04-20 18:07:04 +0300

    Zachet 20.04.22

--- zachet1404/6.py
+++ zachet1404/6.py
@@ -2,13 +2,10 @@ n = int(input("Введите число: "))
 
 dict_money = {64: 0, 32: 0, 16: 0, 8: 0, 4: 0, 2: 0}
 
-for key in dict_money.keys():
+for key in dict_money:
     count = n // key
     if count >= 1:
         dict_money[key] = count
         n -= key * count
 
 print(dict_money, '\n', sum(dict_money.values()))
-
-
-
